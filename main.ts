09835537f9f40d610fca3825a99d53297caff842
IlyasaBashirin <110526670+IlyasaBashirin@users.noreply.github.com>
2023-03-09 10:48:32 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Butterfly)
+basic.forever(function () {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 95)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 40)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 40)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 0)
+    }
+})
